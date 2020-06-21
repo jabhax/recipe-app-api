e3b1ea355ba77890c610d6e23672d0c2f449e44e
jabhax <justiniabarros@gmail.com>
2020-06-21 02:34:02 -0700

    Added recipe image upload feature.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets, mixins
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import viewsets, mixins, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
@@ -7,11 +9,23 @@ from core.models import Tag, Ingredient, Recipe
 from recipe import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin,
-                 mixins.CreateModelMixin):
-    """ Manage Tags in the Database """
+class BaseRecipeAttrViewSet(viewsets.GenericViewSet, mixins.ListModelMixin,
+                            mixins.CreateModelMixin):
+    """ Base viewset for user owned recipe attributes """
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """ Return objects for the current authenticated user only """
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """ Create a new object """
+        serializer.save(user=self.request.user)
+
+
+class TagViewSet(BaseRecipeAttrViewSet):
+    """ Manage Tags in the Database """
     queryset = Tag.objects.all()
     serializer_class = serializers.TagSerializer
 
@@ -24,12 +38,9 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin,
         serializer.save(user=self.request.user)
 
 
-class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin,
-                        mixins.CreateModelMixin):
+class IngredientViewSet(BaseRecipeAttrViewSet):
     """ Manage Ingredients in the Database """
 
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
     queryset = Ingredient.objects.all()
     serializer_class = serializers.IngredientSerializer
 
@@ -57,8 +68,20 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """ Return appropriate serializer class """
         if self.action == 'retrieve':
             return serializers.RecipeDetailSerializer
+        elif self.action == 'upload_image':
+            return serializers.RecipeImageSerializer
         return self.serializer_class
 
     def perform_create(self, serializer):
         """ Create a new recipe """
         serializer.save(user=self.request.user)
+
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        """ Upload an image to a recipe """
+        recipe = self.get_object()
+        serializer = self.get_serializer(recipe, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
